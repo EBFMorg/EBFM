6343afa7fac2d83be7d05a1c5f66e6dcf818c88b
Benjamin Rodenberg <rodenberg@dkrz.de>
2025-11-11 13:26:42 +0100

    Make YAC an optional dependency (#22)
    * Update `pyproject.toml`
        * Add requirement `yac` under extras 'cpl'.
    
    * Make `yac` optional in EBFM
        * Introduce `coupler.py` to provide interface that is independent from `yac`.
        * Explicitly check for options related to coupling. Only `import coupling` if required.
    
    * Refactoring
        * Introduce groups for options.
        
    * Update docs.

--- src/dummy.py
+++ src/dummy.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import coupling
 from pathlib import Path
 import argparse
 
@@ -22,6 +21,16 @@ from mpi4py import MPI
 from utils import setup_logging
 import logging
 
+from typing import List
+
+try:
+    import coupling  # noqa: E402
+
+    coupling_supported = True
+except ImportError as e:
+    coupling_supported = False
+    coupling_import_error = e
+
 log_levels = {
     "file": logging.DEBUG,  # log level for logging to file
     0: logging.INFO,  # log level for rank 0
@@ -33,59 +42,61 @@ setup_logging(log_levels=log_levels)
 logger = logging.getLogger(__name__)
 
 
-def main():
-    parser = argparse.ArgumentParser()
+def add_coupling_arguments(parser: argparse.ArgumentParser):
+    """
+    Add command line arguments related to coupling with other models via YAC.
 
-    parser.add_argument(
+    @param[in] parser the argument parser to add the coupling arguments to.
+    """
+
+    # Note: If you add arguments to this function, also update check_coupling_features.
+
+    coupling_group = parser.add_argument_group("coupling (requires YAC)")
+
+    coupling_group.add_argument(
         "--couple-to-elmer-ice",
         action="store_true",
         help="Enable coupling with Elmer/Ice models via YAC",
     )
 
-    parser.add_argument(
+    coupling_group.add_argument(
         "--couple-to-icon-atmo",
         action="store_true",
         help="Enable coupling with ICON via YAC",
     )
 
-    parser.add_argument(
+    coupling_group.add_argument(
         "--coupler-config",
         type=Path,
         help="Path to the coupling configuration file (YAC coupler_config.yaml).",
     )
 
-    parser.add_argument(
-        "--elmer-mesh",
-        type=Path,
-        help="Path to the Elmer mesh file. Either --elmer-mesh or --matlab-mesh is required.",
-    )
 
-    parser.add_argument(
-        "--matlab-mesh",
-        type=Path,
-        help="Path to the MATLAB mesh file. Either --elmer-mesh or --matlab-mesh is required.",
-    )
+def extract_active_coupling_features(args: argparse.Namespace) -> List[str]:
+    """
+    Determine if coupling is required based on the provided command line arguments.
 
-    parser.add_argument(
-        "--netcdf-mesh",
-        type=Path,
-        help="Path to the NetCDF mesh file. Optional if using --elmer-mesh."
-        " If --netcdf-mesh is provided elevations will be read from the given NetCDF mesh file.",
-    )
+    @param[in] args the parsed command line arguments.
 
-    parser.add_argument(
-        "--is-partitioned-elmer-mesh",
-        action="store_true",
-        help="Indicate if the provided Elmer mesh is partitioned for parallel runs.",
-    )
+    @return a list of argument names that indicate coupling is required.
+    """
 
-    parser.add_argument(
-        "--use-part",
-        type=int,
-        default=MPI.COMM_WORLD.rank + 1,
-        help="If using a partitioned Elmer mesh, allows to specify which partition ID to use for this run. "
-        "If not provided, the MPI rank + 1 will be used as partition ID.",
-    )
+    active_coupling_args = []
+
+    if args.couple_to_elmer_ice:
+        active_coupling_args.append("--couple-to-elmer-ice")
+
+    if args.couple_to_icon_atmo:
+        active_coupling_args.append("--couple-to-icon-atmo")
+
+    if args.coupler_config:
+        active_coupling_args.append("--coupler-config")
+
+    return active_coupling_args
+
+
+def main():
+    parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "--version",
@@ -93,10 +104,66 @@ def main():
         help="Show the EBFM version and exit.",
     )
 
+    input_group = parser.add_argument_group("input mesh types")
+
+    input_group.add_argument(
+        "--elmer-mesh",
+        type=Path,
+        help="Path to the Elmer mesh file. Either --elmer-mesh or --matlab-mesh is required.",
+    )
+
+    input_group.add_argument(
+        "--matlab-mesh",
+        type=Path,
+        help="Path to the MATLAB mesh file. Either --elmer-mesh or --matlab-mesh is required.",
+    )
+
+    input_group.add_argument(
+        "--netcdf-mesh",
+        type=Path,
+        help="Path to the NetCDF mesh file. Optional if using --elmer-mesh."
+        " If --netcdf-mesh is provided elevations will be read from the given NetCDF mesh file.",
+    )
+
+    parallel_group = parser.add_argument_group("parallel runs and distributed meshes")
+
+    parallel_group.add_argument(
+        "--is-partitioned-elmer-mesh",
+        action="store_true",
+        help="Indicate if the provided Elmer mesh is partitioned for parallel runs.",
+    )
+
+    parallel_group.add_argument(
+        "--use-part",
+        type=int,
+        default=MPI.COMM_WORLD.rank + 1,
+        help="If using a partitioned Elmer mesh, allows to specify which partition ID to use for this run. "
+        "If not provided, the MPI rank + 1 will be used as partition ID.",
+    )
+
+    # Add args for features requiring 'import coupling'
+    add_coupling_arguments(parser)
+
     args = parser.parse_args()
 
     if args.version:
         ebfm.print_version_and_exit()
+
+    has_active_coupling_features = extract_active_coupling_features(args)
+    if has_active_coupling_features and not coupling_supported:
+        raise RuntimeError(
+            f"""
+Coupling requested via command line argument(s) {has_active_coupling_features}, but the 'coupling' module could not be
+imported due to the following error:
+
+{coupling_import_error}
+
+Hint: If you are missing 'yac', please install YAC and the python binds as described under
+https://dkrz-sw.gitlab-pages.dkrz.de/yac/d1/d9f/installing_yac.html"
+"""
+        )
+    else:
+        from coupler import NoCoupler
 
     logger.info(f"Starting EBFM version {ebfm.get_version()}...")
 
@@ -123,7 +190,7 @@ def main():
         )
         coupling.setup(coupler, grid["mesh"], time2)
     else:
-        coupler = coupling.NoCoupler("ebfm")
+        coupler = NoCoupler("ebfm")
 
     # Time-loop
     logger.info("Entering time loop...")
