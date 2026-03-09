74357c489e8e68ea6c85ea4a2b6b978bd1dd45b9
Matthias Ibach <ibach@dkrz.de>
2026-03-09 14:26:20 +0100

    Fix guards in main-py for huss and sfcpress

--- src/ebfm/main.py
+++ src/ebfm/main.py
@@ -296,7 +296,7 @@ https://dkrz-sw.gitlab-pages.dkrz.de/yac/d1/d9f/installing_yac.html"
                 "albedo": OUT["albedo"],
             }
 
-            data_from_icon = icon_atmo.exchange(data_to_icon)
+            data_from_icon, errors_from_icon = icon_atmo.exchange(data_to_icon)
 
             logger.debug("Done.")
             logger.debug("Received the following data from ICON:", data_from_icon)
@@ -311,10 +311,10 @@ https://dkrz-sw.gitlab-pages.dkrz.de/yac/d1/d9f/installing_yac.html"
             IN["WS"] = data_from_icon["sfcwind"]
             IN["T"] = data_from_icon["tas"]
             IN["rain"] = IN["P"] - IN["snow"]  # TODO: make this more flexible and configurable
-            # Fallback to constants if fields are not coupled (None); must be arrays for mask indexing.
+            # Fallback to constants if fields are not coupled (error code set); must be arrays for mask indexing.
             _T0 = IN["T"] * 0.0
-            IN["q"] = data_from_icon["huss"] if data_from_icon["huss"] is not None else _T0
-            IN["Pres"] = data_from_icon["sfcpres"] if data_from_icon["sfcpres"] is not None else _T0 + 101500.0
+            IN["q"] = data_from_icon["huss"] if not errors_from_icon["huss"] else _T0
+            IN["Pres"] = data_from_icon["sfcpres"] if not errors_from_icon["sfcpres"] else _T0 + 101500.0
 
         IN, OUT = LOOP_climate_forcing.main(C, grid, IN, t, time, OUT, coupler)
 
