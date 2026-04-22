Fix units for SMB being passed to Elmer/Ice and develop general functionality for unit conversion (#106)
* Tidy up units and unit conversion
    * Fix units for SMB being passed to Elmer/Ice
    * Perform units conversion inside Coupling module

* Improve Coupler module and TimeConfig
    * Move setting self._time and other common functionality into Coupler baseclass.
    * Store TimeConfig in Coupler.
    * Offer functionality for conversion to ISO8601 in TimeConfig.
    * Use Mapping instead of Dict in put operations because Mapping only allows read-only access (checked via mypy) and  this avoids accidental in-place modification of data_to_exchange.
    * Add transformation argument in put/get to allow user more convenient unit conversion via lambda functions.

---------

Co-authored-by: Benjamin Rodenberg <rodenberg@dkrz.de>

src/ebfm/coupling/components/elmer_ice.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from typing import TYPE_CHECKING
+from collections.abc import Mapping
 import numpy as np
 
 if TYPE_CHECKING:
@@ -10,7 +11,9 @@ if TYPE_CHECKING:
 
 from .base import Component
 
-from ebfm.coupling.fields import FieldSet, Field, ExchangeType, days_to_iso
+from ebfm.coupling.fields import FieldSet, Field, ExchangeType, Timestep
+from ebfm.core.config import TimeConfig
+from ebfm.core.constants import DAYS_PER_YEAR
 
 
 class ElmerIce(Component):
@@ -23,11 +26,11 @@ class ElmerIce(Component):
     def __init__(self, coupler: "Coupler"):
         super().__init__(coupler)
 
-    def get_field_definitions(self, time: dict[str, float]) -> FieldSet:
+    def get_field_definitions(self, time: TimeConfig) -> FieldSet:
         """
         Get generic field definitions for EBFM coupling to Elmer/Ice.
         """
-        timestep = days_to_iso(time["dt"])
+        timestep = Timestep(value=time.time_step_iso8601())
 
         return FieldSet(
             {
@@ -75,20 +78,27 @@ class ElmerIce(Component):
             }
         )
 
-    def exchange(self, data_to_exchange: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+    def exchange(self, data_to_exchange: Mapping[str, np.ndarray]) -> dict[str, np.ndarray]:
         """
         Exchange data with Elmer/Ice.
 
-        @param[in] data_to_exchange dictionary of field names and their data to be sent
+        @param[in] data_to_exchange read-only Mapping of field names to data to be sent
 
         @returns dictionary of received field data
         """
         received_data: dict[str, np.ndarray] = {}
 
+        # For fields representing rates (e.g. SMB, runoff), we need to convert them from per timestep to per year
+        # before sending to Elmer/Ice, which expects annual values.
+        def map_per_timestep_to_per_year(x_per_timestep: np.ndarray) -> np.ndarray:
+            x_per_day = x_per_timestep / self._coupler.get_time_step_in_days()
+            x_per_year = x_per_day * DAYS_PER_YEAR
+            return x_per_year
+
         # Put data to Elmer/Ice
         self._put_if_coupled("T_ice", data_to_exchange)
-        self._put_if_coupled("smb", data_to_exchange)
-        self._put_if_coupled("runoff", data_to_exchange)
+        self._put_if_coupled("smb", data_to_exchange, transform=map_per_timestep_to_per_year)
+        self._put_if_coupled("runoff", data_to_exchange, transform=map_per_timestep_to_per_year)
 
         # Get data from Elmer/Ice
         h = self._get_if_coupled("h")

src/ebfm/coupling/components/icon_atmo.py
@@ -3,14 +3,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from typing import TYPE_CHECKING
+from collections.abc import Mapping
 import numpy as np
+from ebfm.core.constants import SECONDS_PER_DAY
 
 if TYPE_CHECKING:
     from ebfm.coupling.couplers.base import Coupler
 
 from .base import Component
 
-from ebfm.coupling.fields import FieldSet, Field, ExchangeType, days_to_iso
+from ebfm.coupling.fields import FieldSet, Field, ExchangeType, Timestep
+from ebfm.core.config import TimeConfig
 
 
 class IconAtmo(Component):
@@ -23,11 +26,11 @@ class IconAtmo(Component):
     def __init__(self, coupler: "Coupler"):
         super().__init__(coupler)
 
-    def get_field_definitions(self, time: dict[str, float]) -> FieldSet:
+    def get_field_definitions(self, time: TimeConfig) -> FieldSet:
         """
         Get generic field definitions for EBFM coupling to IconAtmo.
         """
-        timestep = days_to_iso(time["dt"])
+        timestep = Timestep(value=time.time_step_iso8601())
 
         return FieldSet(
             {
@@ -104,24 +107,33 @@ class IconAtmo(Component):
             }
         )
 
-    def exchange(self, data_to_exchange: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+    def exchange(self, data_to_exchange: Mapping[str, np.ndarray]) -> dict[str, np.ndarray]:
         """
         Exchange data with IconAtmo.
 
-        @param[in] data_to_exchange dictionary of field names and their data to be sent
+        @param[in] data_to_exchange read-only Mapping of field names to data to be sent
 
         @returns dictionary of received field data
         """
         received_data: dict[str, np.ndarray] = {}
 
+        # We need to convert precipitation received from ICON from kg / m^2 / s
+        # to m w.e. (per EBFM timestep)
+        def map_pr_to_ebfm(precipitation: np.ndarray) -> np.ndarray:
+            mwe_per_second = precipitation * 1e-3
+            mwe_per_day = mwe_per_second * SECONDS_PER_DAY
+            mwe_per_timestep = mwe_per_day * self._coupler.get_time_step_in_days()
+            return mwe_per_timestep
+
         # Put data to IconAtmo
         self._put_if_coupled("albedo", data_to_exchange)
 
         # Get data from IconAtmo
-        pr = self._get_if_coupled("pr")
+        pr = self._get_if_coupled("pr", transform=map_pr_to_ebfm)
         if pr is not None:
             received_data["pr"] = pr
 
+        # TODO: check what units are needed for pr_snow in EBFM
         pr_snow = self._get_if_coupled("pr_snow")
         if pr_snow is not None:
             received_data["pr_snow"] = pr_snow